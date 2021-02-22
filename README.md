82879f045ff9b3936d794b6d4bd8584e0381469e
sby <l>
2021-02-22 15:30:14 +0800

    chore: pug config

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # html-template
 
 
-git reset --soft HEAD^ 撤销commit
+git reset --soft HEAD^ 撤销commit，但是保留修改的文件
+
+
+
+
+### 更新
+git pull  多人合作时，拉取最新代码
+
+
+### 保存
+git add .  // 只要有修改都要
+git commit -m 'chore: pug功能完成'
+
+
+### 提交
+git push
--- gulp.js
+++ gulp.js
@@ -8,6 +8,45 @@
  *  6. 提供 gulp build命令
  */
 
-function pug() {
+const {
+    series,
+    parallel,
+    watch,
+    src,
+    dest
+} = require('gulp')
+const pug = require('gulp-pug') // 编译pug
+prettify = require('gulp-prettify') // 美化html
+const stylus = require('gulp-stylus') // 编译stylus
+const browserSync = require('browser-sync').create() // 自动刷新
+const {reload} = browserSync
+
+// 注：函数名不能和引入的插件变量名称重复
+
+function trans_pug() { 
     // todo
+    return src('pug/**/*.pug') // pug 目录下所有的.pug文件,含各种文件夹中的.pug文件
+        .pipe(pug({
+            pretty: true,
+            self: true, // 是否使用一个叫做 self 的命名空间来存放局部变量
+        }))
+        .pipe(prettify({
+            indent_size: 4,
+        }))
+        .pipe(dest('../'))
 }
+
+function browser_sync(cb) {
+    browserSync.init({
+        server: {
+            baseDir: path.resolve('..', './')
+        }
+    })
+    // 4.0新版本直接引入watch即可实现任务监听功能,不用安装插件,series也可以配合watch按顺序执行设置的任务函数,注意这里不需要使用return
+    watch('pug/**', {
+        events: 'all',
+        ignoreInitial: false
+    }, trans_pug)
+}
+
+exports.build = browser_sync()
